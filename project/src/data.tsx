b766a2e68f73d668ecf9d778209660db62cb3204
Andrey Kosov < emailAndrejcosov@yandex.ru>
2022-02-19 23:51:12 +0300

    создал компоненты

---
+++ project/src/data.tsx
@@ -0,0 +1,5 @@
+import MovieCard from './components/movie-card-screen/movie-card-screen';
+
+const mockFilms = new Array(20).fill(<MovieCard/>);
+
+export default mockFilms;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const movieData = {
+  title: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  year: 2014,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      title = {movieData.title}
+      genre = {movieData.genre}
+      year = {movieData.year}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
