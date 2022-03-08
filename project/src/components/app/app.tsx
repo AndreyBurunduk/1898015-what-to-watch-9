39f49304e0db34cea9f478e190e5f566770d3ef4
Andrey Kosov < emailAndrejcosov@yandex.ru>
2022-03-08 16:51:27 +0300

    правка

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -2,13 +2,13 @@ import { AppRoute, AuthorizationStatus } from '../../const';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import MainPage from '../main-page/main-page';
-import ReviewPage from '../review-page/review-page';
-import PlayerPage from '../player-page/player-page';
-import SignInPage from '../sing-in-page/sing-in-page';
 import MoviePage from '../movie-page/movie-page';
 import MyListPage from '../my-list-page/my-list-page';
-import PrivateRoute from '../private-route/private-route';
 import NotFoundPage from '../not-found-page/not-found-page';
+import PlayerPage from '../player-page/player-page';
+import PrivateRoute from '../private-route/private-route';
+import ReviewPage from '../review-page/review-page';
+import SignInPage from '../sing-in-page/sing-in-page';
 
 type PropsTypes = {
   promoProps: {
