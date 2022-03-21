1433261b1edefd05932825f0efbcb472ed1ca8a6
Andrey Kosov < emailAndrejcosov@yandex.ru>
2022-03-21 23:28:52 +0300

    redux

--- project/src/components/small-film-card/small-film-card.tsx
+++ project/src/components/small-film-card/small-film-card.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import FilmCard from '../film-card/film-card';
 import { FilmType } from '../../types/types';
 import PlayerCart from '../player-cart/player-cart';
+import GenreFilmFilter  from '../../utils/genre-film-filter';
 
 type SmallFilmCardProps = {
   catalogFilms: FilmType[];
@@ -20,10 +21,9 @@ function SmallFilmCard({ catalogFilms }: SmallFilmCardProps): JSX.Element {
     setFilmCartInfo(null);
   };
 
-
   return (
     <>
-      {catalogFilms.map((film) => (
+      {GenreFilmFilter(catalogFilms).map((film: FilmType) => (
         <FilmCard
           key={film.id}
           catalogFilm={film}
